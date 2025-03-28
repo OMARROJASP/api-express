a868c6bb341e44da642d7f44254b692c96761506
OMARROJASP <63993140+OMARROJASP@users.noreply.github.com>
2025-03-28 12:53:09 -0500

    nuevos cambios de crud

---
+++ src/controllers/item.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from "express"
+import { handleHttp } from "../../utils/error.handler"
+
+const getItem = (req:Request, res:Response) => {
+    try{
+        
+    }catch(e){
+       handleHttp(res, 'ERROR_GET_ITEM');
+    }
+
+}
+
+const getItems = (req:Request, res:Response) => {
+    try{
+
+    }catch(e){
+       handleHttp(res, 'ERROR_GET_ITEMS');
+    }
+}
+
+const updateItem = (req:Request, res:Response) => {
+    try{
+
+    }catch(e){
+       handleHttp(res, 'ERROR_UPDATE_ITEMS');
+    }
+}
+
+const postItem = (req:Request, res:Response) => {
+    try{
+
+    }catch(e){
+       handleHttp(res, 'ERROR_POST_ITEMS');
+    }
+}
+
+const deleteItem = (req:Request, res:Response) => {
+    try{
+
+    }catch(e){
+       handleHttp(res, 'ERROR_DELETE_ITEMS');
+    }
+}
+
+export { getItem, getItems, updateItem, postItem, deleteItem};
