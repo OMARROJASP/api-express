891307625491ba83f7e998f97a9dcb0a84aff800
OMARROJASP <63993140+OMARROJASP@users.noreply.github.com>
2025-03-29 10:42:18 -0500

    crete blog

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import db from "./config/mongo";
 const PORT =process.env.PORT || 3001;
 const app = express();
 app.use(cors());
+app.use(express.json());
 app.use(router);
-app.use(express.json())
 db().then(() => console.log('Conexion Ready'));
 app.listen(PORT, () => console.log(`listo por el puerto ${PORT}`))
