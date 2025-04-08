5ce637eca6ddea36038a461c66e18336ae45528a
OMARROJASP <63993140+OMARROJASP@users.noreply.github.com>
2025-04-07 23:36:20 -0500

    add auth interface

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,11 @@
 import { Request, Response, Router } from "express";
-
+const { registerCthrl, loginCthrl } = require("../controllers/auth");
 
 /**
  http//localhost:3002/auth/register [POST]
  */
 const router = Router();
-router.post("/register" );
-router.post("/login" );
+router.post("/register", registerCthrl );
+router.post("/login",loginCthrl );
 
 export { router };
